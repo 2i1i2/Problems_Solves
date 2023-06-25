d0ec31268a65af52a8f3ed4ae6267ee872d3990d
2i1i2 <nitinkumar42045@gmail.com>
2023-06-25 22:23:34 +0530

    added extension and gererice method

---
+++ AdvancedcSharp/Extension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedcSharp
+{
+    internal class Extension
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is suppose to very long string";
+            var shortString = str.Shorten(5);
+            Console.WriteLine(shortString);
+            Console.ReadLine();
+        }
+
+
+    }
+    public static class StringExtension
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfRange should be greater than or equal to 0.");
+            if (numberOfWords == 0)
+                return "";
+            var words = str.Split(' ');
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords));
+        }
+    }
+}
---
+++ AdvancedcSharp/GenericSwap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedcSharp
+{
+    internal class GenericSwap
+    {
+        static void Main(string[] args)
+        {
+            string a = "abc";
+            string b = "xyz";
+            Console.WriteLine($"{a} {b}");
+            SwapGenericMethod<string>(ref a,ref b);
+            Console.WriteLine($"{a} {b}");
+            Console.ReadLine();
+        }
+
+
+        static void SwapGenericMethod<T>(ref T a,ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
